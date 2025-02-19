0a011fa0f93812564efee971d6bb77da0fedc229
Tyler Gobka Bright <tgbright88@gmail.com>
2025-02-19 14:01:47 +0000

    developing...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/home/logo.png";
 const Header = () => {
   return (
     <div className="flex items-center justify-between mb-10 md:-ml-4 px-4 py-2">
-      <a href="/" >
+      <a href="/">
         <img src={logo} alt="Shakes and Dessert" className="w-10 md:w-14" />
       </a>
       <p className="text-xs text-gray-200 font-semibold">Shakes & Dessert</p>
